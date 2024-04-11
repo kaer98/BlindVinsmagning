1088c603798d85fce6840f0c75fb555af6baae3a
Seymen Kiran <seki48835@edu.ucl.dk>
2024-04-11 14:39:10 +0200

    updated connection so its in another file and imported as method

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -2,20 +2,21 @@ import express from 'express';
 import { Sequelize } from 'sequelize';
 import dotenv from 'dotenv';
 import tastingRoutes from './routes/tastings.routes.js';
+import connectToMySQL from "./db/connectToMySQL.js";
+
 
 if (process.env.IN_DOCKER == undefined) {
-  dotenv.config()
+  dotenv.config();
 }
 
 //Oprettelse af Express appen
-const app = express()
+const app = express();
 
 //Environmental Variables sektion
 //Man skal installere NPM Package 'dotenv' for at kunne bruge environmental variables i Node.js.
 
 //Env Variables fungerer ikke på nuværende tidspunkt, skal fixes
-const DB_STRING = process.env.DB_STRING// || 'mysql://wine:i4g5WNxi5KBr8s@hjem.jazper.dk:3306/WINE_DB';
-const PORT = 3001;
+const PORT = process.env.PORT || 3000;
 
 
 //Middleware - Etc
@@ -24,21 +25,16 @@ app.use(express.json()); // Til at parse inkommende requests med JSON Payloads. 
 //Middleware - Routes
 app.use("/api/tastings", tastingRoutes);
 
-//Burde nok laves som environmental variable
-
-//Connection String
-const sequelize = new Sequelize(DB_STRING);
-
-try {
-  await sequelize.authenticate();
-  console.log('Connection to DB Sucessfull');
-} catch (error) {
-  console.error('Connection to DB Failed', error);
-}
-
-
-
 
 app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT})...`);
-});
+
+    //DB forbindelse
+    connectToMySQL();
+    
+    console.log(`(Listening on port ${PORT})...`);
+  });
+
+
+
+
+
