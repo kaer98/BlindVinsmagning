24954d65a5eecd3dd2ff72d86dfb1ecdf817d5c1
joakim okkels <joakim.okkels@gmail.com>
2024-04-11 14:11:51 +0200

    visibility

--- appr/lib/models/winetasting.dart
+++ appr/lib/models/winetasting.dart
@@ -1,12 +1,12 @@
 import 'package:appr/models/evaluation.dart';
 import 'package:appr/models/wine.dart';
 
-enum Winevisenum { blind, semi, open }
+enum Visibilityenum { blind, semi, open }
 
 class Winetast {
 Wine wine;
 String host, participents;
-Winevisenum? winevis; 
+Visibilityenum? visibility; 
 Evaluation evaluation;
 DateTime date;
 bool status;
@@ -16,7 +16,7 @@ bool status;
  Winetast({
    required this.wine,
    required this.date,
-   required this.winevis,
+   required this.visibility,
    required this.host,
    required this.participents,
    required this.evaluation,
