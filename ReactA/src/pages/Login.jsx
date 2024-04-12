e604faa7fde7957e5c75d3c85ae6605324da8e2a
AdilJavatogo <Adil2300@hotmail.com>
2024-04-12 11:51:41 +0200

    login, main page, routing

--- ReactA/src/pages/Login.jsx
+++ ReactA/src/pages/Login.jsx
@@ -1,3 +1,22 @@
-export default function Home(){
-    return <div>Login Page</div>
+import styles from "../css/login.module.css";
+import "../css/App.css";
+import { Link } from "react-router-dom";
+
+export default function Login() {
+  return (
+    <div className={styles.loginContainer}>
+      <div className={styles.login}>
+        <div className={styles.text}>Login Page</div>
+        <Link to="/main">
+          <button>Login</button>
+        </Link>
+        <Link to="/">
+          <button>Registre</button>
+        </Link>
+        <Link to="/main">
+          <button>Anonym</button>
+        </Link>
+      </div>
+    </div>
+  );
 }
