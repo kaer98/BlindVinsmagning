00c26d83595626e18ceb517a1b70a52576c5a50a
AdilJavatogo <Adil2300@hotmail.com>
2024-04-15 15:39:27 +0200

    join session page + bootstrap

--- ReactA/src/pages/Login.jsx
+++ ReactA/src/pages/Login.jsx
@@ -1,10 +1,9 @@
 import styles from "../css/login.module.css";
-import "../css/App.css";
 import { Link } from "react-router-dom";
 
 export default function Login() {
   return (
-    <div className={styles.loginContainer}>
+    <div className="container">
       <div className={styles.login}>
         <div className={styles.text}>Login Page</div>
         <Link to="/main">
