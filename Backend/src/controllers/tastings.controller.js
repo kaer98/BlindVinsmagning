790805ba67b43c2b50713c6b29d7ea2a8c18a3fa
Jasper Christiansen <jasper@jazper.dk>
2024-04-16 14:08:59 +0200

    forkert backend

--- Backend/src/controllers/tastings.controller.js
+++ Backend/src/controllers/tastings.controller.js
@@ -3,21 +3,27 @@ Controller for tasting routen
 
 */
 
+// model WineTasting {
+//     id    Int     @id @default(autoincrement())
+//     title String? @db.VarChar(255)
+  
+//     visibility               WineTasting_visibility?
+//     date                     DateTime?                  @db.Date
+//     hostId                   Int?
+//     winnerId                 Int?
+//     finished                 Boolean?
+//     User                     User?                      @relation(fields: [hostId], references: [id], onDelete: NoAction, onUpdate: NoAction, map: "WineTasting_ibfk_1")
+//     Wine                     Wine?                      @relation(fields: [winnerId], references: [id], onDelete: NoAction, onUpdate: NoAction, map: "WineTasting_ibfk_2")
+//     WineTasting_Participants WineTasting_Participants[]
+//     WineTasting_Wines        WineTasting_Wines[]
+  
+//     @@index([hostId], map: "hostId")
+//     @@index([winnerId], map: "winnerId")
 
 //GET Request: Hent smagninger ned så deltagere kan se kommende smagninger i appen
 export const getTastings = async (request, response) => {
     try {
-        const allTastings = await prisma.winetasting.findMany({
-            select: {
-                id: true,
-                name: true,
-                date: true,
-                hostId: true,
-                winnerId: true,
-                finished: false
-            }
-    
-        });
+        const allTastings = await prisma.winetasting.findMany();
 
         response.json(allTastings);
 
@@ -30,7 +36,6 @@ export const getTastings = async (request, response) => {
 
 //POST Request: Oprettelse af smagning (Kun for administratorer)
 export const createTasting = async (request, response) => {
-
 
 }
 
