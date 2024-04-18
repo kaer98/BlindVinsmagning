af2452abef294cf0198811b9996b217b79aaff47
Seymen Kiran <seki48835@edu.ucl.dk>
2024-04-18 16:33:42 +0200

    Added join tasting feature

--- Backend/src/routes/tastings.route.ts
+++ Backend/src/routes/tastings.route.ts
@@ -1,14 +1,17 @@
 import {db} from '../drizzle/db';
 import express from "express";
 import protectRoute from '../middleware/protectRoute';
-import { createTasting, getAllTastings } from '../controllers/tastings.controller';
+import { createTasting, getAllTastings, joinTasting } from '../controllers/tastings.controller';
 
 const router = express.Router();
 
-//ROUTE: GET /api/tastings
+//ROUTE: GET /api/tastings (Alle kan se smagninger)
 router.get("/",  getAllTastings);
 
-//ROUTE: POST /api/tastings
+//ROUTE: POST /api/tastings (Man skal være logget ind)
 router.post("/", protectRoute, createTasting);
+
+//ROUTE: GET /api/tastings/join/:id (Man skal være logget ind)
+router.get("/join/:id", protectRoute, joinTasting);
 
 export default router;
