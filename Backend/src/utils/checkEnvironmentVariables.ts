e8a69b227740ef5cbf71351128ae9fad1ddbe76a
Jasper Christiansen <jasper@jazper.dk>
2024-04-18 13:19:27 +0200

    Fix typo

--- Backend/src/utils/checkEnvironmentVariables.ts
+++ Backend/src/utils/checkEnvironmentVariables.ts
@@ -5,7 +5,7 @@ const checkEnvironmentVariables = () => {
     }
 
     if (process.env.JWT_SECRET == undefined) {
-        console.log('Missing "DATABASE_URL" Environment Variable');
+        console.log('Missing "JWT_SECRET" Environment Variable');
         process.exit()
     }
 };
