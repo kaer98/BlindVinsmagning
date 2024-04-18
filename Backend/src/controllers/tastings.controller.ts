a43abbc75eed10b94d26ceecacddb8c773e20b74
Seymen Kiran <seki48835@edu.ucl.dk>
2024-04-18 14:16:07 +0200

    pushed latest tastings not work

--- Backend/src/controllers/tastings.controller.ts
+++ Backend/src/controllers/tastings.controller.ts
@@ -4,81 +4,51 @@ import { winetastings } from "../drizzle/schema";
 import type { Request, Response } from "express";
 
 
+
 export const createTasting = async (request: Request, response: Response) => {
     try {
-
-        //Disse er de data som serveren skal bruge til når en bruger skal oprette en ny bruger.
-        //Dem får man fra Request bodien. Matchende ting (som fulName fx) gemmes hvor de skal gemmes.
+        // Disse er de data som serveren skal bruge til når en bruger skal oprette en ny bruger.
+        // Dem får man fra Request bodien. Matchende ting (som fulName fx) gemmes hvor de skal gemmes.
         const { name, visibility, date, wines } = request.body;
+
+        console.log(name, visibility, date, wines);
 
         // Validering af Request Body
         if (!name || !visibility || !date || !wines) {
             return response.status(400).json({ error: 'Ugyldige requests fra Request body.' });
         }
 
-
-
-        //Her ser man om vinen allerede eksisterer i databasen. 
+        // Her ser man om vinen allerede eksisterer i databasen. 
         const tastingToFind = await db.query.winetastings.findFirst({
             where: eq(winetastings.name, name),
         });
 
-
-        //Hvis denne brugernavn og bruger eksisterer i databasen, må man ikke oprette sig under samme navn.
+        // Hvis denne smagning eksisterer i databasen, må man ikke oprette en med samme navn.
         if (tastingToFind) {
             return response.status(400).json({ error: "Smagning med dette navn eksisterer allerede" });
         }
 
+        // Den nye smagning oprettes i første omgang på serveren (som objekt)
+        const newTastingCreation = await db.insert(winetastings).values({
+            name: name,
+            visibility: visibility,
+            date: new Date(date).toString(),
+            hostid: 1,
+            winnerid: 1,
+            finished: false,
+            participants: [1],
+            wines: wines
+        });
 
-        //Den nye bruger oprettes i første omgang på serveren (som objekt)
-   
-
-        //Hvis alt er vel oprettes der en JWT token og brugeren gemmes i databasen.
-       
-
-            const newTastingCreation = await db.insert(winetastings).values({
-                
-                // name: name,
-                // visibility: visibility,
-                // date: date.toString(),
-                // wines: parseInt(wines),
-                // hostid: "1",
-                name: name,
-                visibility: visibility,
-                date: date,
-                hostid: 1,
-                finished: false, 
-                participants: [1],
-                wines: wines,
-                winnerid: 1,
-                
-                
-                
-
-
-            }).returning({
-                // wineName: winetastings.name,
-                // id: winetastings.id,
-                // wineRegion: wines.region,
-           
-            });
-
-            // const newWine = newWineCreation[0];
-            if (newTastingCreation) {
-            response.status(201).json({ created: "yes"});
-            }
-            else {
-                response.status(500).json({ fejl: "Blev ikke oprettet" });
-            }
-
-
-    } catch (error) { //I tilfælde af server fejl.
+        if (newTastingCreation) {
+            response.status(201).json({ created: "yes" });
+        } else {
+            response.status(500).json({ fejl: "Blev ikke oprettet" });
+        }
+    } catch (error) { // I tilfælde af server fejl.
         console.log(error);
         response.status(500).json({ fejl: "BAD!", error: error });
     }
-
-
-
 }
  
 
