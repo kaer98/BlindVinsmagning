9493161eb2a4a5aa2a951087aee21c2c1a1fb8b6
Seymen Kiran <seki48835@edu.ucl.dk>
2024-04-22 10:00:58 +0200

    gg

--- Backend/src/controllers/evaluations.controller.ts
+++ Backend/src/controllers/evaluations.controller.ts
@@ -6,6 +6,9 @@ import { eq } from "drizzle-orm";
 export const getAllEvaluations = async (request: Request, response: Response) => {
     try {
 
+        db.query.evaluations.findMany().then((evaluations) => {
+            response.json(evaluations);
+        });
 
 
     } catch (error) {
@@ -16,10 +19,10 @@ export const getAllEvaluations = async (request: Request, response: Response) =>
 
 export const createEvaluation = async (request: Request, response: Response) => {
     try {
-        const { name, wineId, tastingId  } = request.body;
+        const { name, wineId, tastingId } = request.body;
 
         // Validering af Request Body
-        if (  !name || !wineId || !tastingId) {
+        if (!name || !wineId || !tastingId) {
             return response.status(400).json({ error: 'Ugyldige requests fra Request body.' });
         }
 
@@ -57,4 +60,58 @@ export const createEvaluation = async (request: Request, response: Response) => 
 
     }
 
+}
+
+export const createAndAddWset = async (request: Request, response: Response) => {
+
+    try {
+        //Create WSET and add the WSET to the Evaluation that matches the USer ID
+        const { aIntensity, nIntensity, sweetness, aromacharacteristics, acidity,
+            tannin, alcohol, body, flavourintensity, flavourcharacteristics, finish, quality } = request.body;
+
+            const tastingId = request.params.id;
+            const parsedTastingId = parseInt(tastingId);
+
+
+        // Validering af Request Body
+        if (!tannin || !nIntensity || !sweetness || !acidity || !aromacharacteristics ||
+            !alcohol || !body || !flavourintensity || !flavourcharacteristics || !finish || !quality) {
+            return response.status(400).json({ error: 'Ugyldige requests fra Request body.' });
+        }
+
+        if (!request.user) {
+            return response.status(401).json({ error: 'Du skal være logget ind for at oprette en vurdering' });
+        }
+
+        const userId = request.user.id;
+
+        const createWset = await db.insert(wset).values({
+            aintensity: aIntensity, nintensity: nIntensity, sweetness, aromacharacteristics, acidity,
+            tannin, alcohol, body, flavourintensity, flavourcharacteristics, finish, quality
+
+        }).returning({
+
+            id: wset.id,
+
+        });
+
+        const newWset = createWset[0];
+
+        const updateEvaluation = await db.update(evaluations).set({
+
+            wsetid: newWset.id
+
+        }).where(( eq(evaluations.userid, userId), eq(evaluations.tastingid, parsedTastingId)));
+
+        if (createWset) {
+            response.status(201).json({ message: `WSET with ID: ${newWset.id} - Created and added to your Evaluation!` });
+        } else {
+            response.status(500).json({ fejl: "WSET Blev ikke oprettet" });
+        }
+
+
+
+    } catch (error) {
+
+    }
 }
